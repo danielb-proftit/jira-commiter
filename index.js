16fad9a908b5f85363f24bc5ec5ab85dfb48a4ed
Daniel Baron <danielb@proftit.com>
2020-11-03 12:00:18 +0200

    CRM-8899 🚧	Commiting now works

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const { exec } = require('child_process');
-exec('git rev-parse --abbrev-ref HEAD', (err, result, stderr) => {
+const readline = require('readline');
+
+exec('git rev-parse --abbrev-ref HEAD', async(err, branchName, stderr) => {
     if (err) {
         // handle your error
     }
 
-    console.log('result', result);
-    console.log('type of result', typeof result);
+    const anchorString = `CRM-`;
+    const anchorStringIndex = branchName.indexOf(anchorString);
+    const crmTicketId = branchName.substring(anchorStringIndex, branchName.indexOf('/', anchorStringIndex));
+    const commitPrefix = `${crmTicketId} 🚧`;
+
+    const commitMessage = await askQuestion("Your commit message:");
+    const fullCommitMessage = `${commitPrefix}\t${commitMessage}`;
+    console.log('fullCommitMessage', fullCommitMessage);
+    // exec(`git commit -m "${fullCommitMessage}"`, (err, result, stderr) => {
+    exec(`git commit -m "${fullCommitMessage}"`, (err, result, stderr) => {
+       if (err){
+           console.log('Error: ', err);
+       }
+       console.log('result', result);
+    });
 });
+
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
+}
