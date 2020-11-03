aeafc05c1878f45369a7cea2b1888e437dc85d6d
Daniel Baron <danielb@proftit.com>
2020-11-03 12:08:59 +0200

    CRM-8899 🚧	refactor

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const { exec } = require('child_process');
-const readline = require('readline');
+const { askQuestion } = require('./ask-question');
+const { getCommitPrefix } = require('./get-commit-prefix');
+const { printError } = require('./print-error');
 
 exec('git rev-parse --abbrev-ref HEAD', async(err, branchName, stderr) => {
     if (err) {
-        // handle your error
+        printError(err);
     }
-
-    const anchorString = `CRM-`;
-    const anchorStringIndex = branchName.indexOf(anchorString);
-    const crmTicketId = branchName.substring(anchorStringIndex, branchName.indexOf('/', anchorStringIndex));
-    const commitPrefix = `${crmTicketId} 🚧`;
-
+    const commitPrefix = getCommitPrefix(branchName);
     const commitMessage = await askQuestion("Your commit message:");
+
     const fullCommitMessage = `${commitPrefix}\t${commitMessage}`;
-    console.log('fullCommitMessage', fullCommitMessage);
-    // exec(`git commit -m "${fullCommitMessage}"`, (err, result, stderr) => {
     exec(`git commit -m "${fullCommitMessage}"`, (err, result, stderr) => {
        if (err){
-           console.log('Error: ', err);
+           printError(err);
        }
-       console.log('result', result);
+       // will print default git commit message (how many lines were inserted or deleted)
+       console.log(result);
     });
 });
-
-function askQuestion(query) {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-
-    return new Promise(resolve => rl.question(query, ans => {
-        rl.close();
-        resolve(ans);
-    }))
-}
